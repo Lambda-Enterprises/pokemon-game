71e538602560d5602626ed187799831108ac3ca1
Xivius <nsong4@hawk.iit.edu>
2018-08-02 09:25:37 -0500

    Deleted unneeded files; prepare to restructure software

--- PokemonMain/Moves/Move.py
+++
@@ -1,8 +0,0 @@
-class Move:
-    def __init__(self, name, type, power, accuracy):
-        self.name = name
-        self.type = type
-        self.power = power
-        self.accuracy = accuracy
-    
-    
