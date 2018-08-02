b7bf259927b43f56761684a47dbbdf077f91ddc8
Xivius <nsong4@hawk.iit.edu>
2018-08-01 22:38:06 -0500

    Added Move Classes

--- PokemonMain/PhysicalMoves.py
+++
@@ -1,7 +0,0 @@
-from Pokemon import *
-
-class PhysicalMove: #class calculates damage value from attacks
-    def __init__(self, attacker, defender, weather): #initializes variables that are given in the class
-        self.attacker = attacker
-        self.defender = defender
-        self.weather = weather
