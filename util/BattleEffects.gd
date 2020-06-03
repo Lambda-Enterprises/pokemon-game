11486e01652b6a7a3c1806c1dcabd1d8d026d9b3
puy-ion <34848177+puy-ion@users.noreply.github.com>
2020-06-02 22:05:39 -0400

    Add files via upload

--- util/BattleEffects.gd
+++ util/BattleEffects.gd
@@ -16,6 +16,44 @@ class BattleEffects:
 	func calcStatChange(attacker, defender):
 		return 0 # to do
 	
+	func burned(pkmn): #incomplete
+		var original_atk = pkmn.atk
+		var burn_atk = pkmn.atk/2
+		if pkmn.status == "burned":
+			pkmn.atk = burn_atk
+			pkmn.hp -= int(pkmn.hp/16)
+		else:
+			pkmn.atk = original_atk
+		
+	func paralysis(pkmn): #incomplete
+		randomize()
+		var orignal_spe = pkmn.spe
+		var para_spe = pkmn.spe / 2
+		if pkmn.status == "paralysis":
+			#0-1 random int number
+			var paralyzed = randi()%1
+			pkmn.spe = para_spe
+		else:
+			pkmn.spe = orignal_spe
+	
+	func confusion(pkmn): #incomplete
+		randomize()
+		var hit = randi()%1
+		var snapout = randi()%2
+		if snapout == 2:
+			pkmn.status = ""
+		elif pkmn.status == "confusion" and hit == 1:
+			#0-1 random int number
+			#hits themselves
+			self.baseDamage(pkmn, pkmn)
+		
+	func poisoned(pkmn): #incomplete
+		pkmn.hp -= int(pkmn.hp/16)
+		
+	func badly_poisoned(pkmn): #incomplete
+		var dmg_ratio = 1
+		pkmn.hp -= int((pkmn.hp*dmg_ratio)/16)
+	
 	func statusCondition(name):
 		var status = {
 			"asleep": 25,
