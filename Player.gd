f68b9ee372c629eb6bdf0b5ffc4d4ced7137141a
Xivius <nsong4@hawk.iit.edu>
2020-06-04 02:44:35 -0500

    Insert small edit

--- Player.gd
+++ Player.gd
@@ -12,9 +12,9 @@ var pokemon = js.accessPokemon(1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_animation(pokemon.name)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
