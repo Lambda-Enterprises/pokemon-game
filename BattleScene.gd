477fceb489bf6476884a71de21e80b3667ee1659
puy-ion <34848177+puy-ion@users.noreply.github.com>
2020-06-14 19:02:00 -0400

    Add files via upload

--- BattleScene.gd
+++ BattleScene.gd
@@ -5,7 +5,6 @@ extends TextureRect
 const BattleEffects = preload("res://util/BattleEffects.gd")
 onready var move_buttons = [$Move1, $Move2, $Move3, $Move4]
 
-
 # Declare member variables here. Examples:
 var turn = ["player", 0]
 
@@ -15,7 +14,7 @@ func _ready():
 #	for i in range(len(move_buttons)):
 #		move_buttons[i].connect("pressed", self, "selected")
 	for i in range(len(move_buttons)):
-		move_buttons[i].text = Global.player.move1.name
+		move_buttons[i].text = Global.player.moves[i].name
 		
 	$Opponent.set_animation(Global.opponent.name)
 	$Player.set_animation(Global.player.name)
@@ -26,11 +25,47 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# warning-ignore:unused_argument
 func _process(delta):
-	if turn[0] == "player":
-		pass
-		#print(turn)
-	else:
-		pass
-		#print(turn)
+	randomize()
+	
 		
+				
+	if turn[0] == "opponent" and turn[1] != 2:
+		BattleEffects.baseDamage(Global.opponent, Global.player, Global.opponent.moves[randi() % 3])
+		turn[1] += 1
+		turn[0] = "player"
+		#print(turn)
+	if turn[1] == 2:
+		turn[1] = 0
+		print("player hp:" + str(Global.player.hp))
+		print("opponent hp:" + str(Global.opponent.hp))
+
+
+func _on_Move1_pressed():
+	if turn[0] == "player" and turn[1] != 2:
+		BattleEffects.baseDamage(Global.player, Global.opponent, Global.player.moves[0])
+		turn[1] += 1
+		turn[0] = "opponent"
+
+
+func _on_Move2_pressed():
+	if turn[0] == "player" and turn[1] != 2:
+		BattleEffects.baseDamage(Global.player, Global.opponent, Global.player.moves[1])
+		turn[1] += 1
+		turn[0] = "opponent"
+
+
+
+func _on_Move3_pressed():
+	if turn[0] == "player" and turn[1] != 2:
+		BattleEffects.baseDamage(Global.player, Global.opponent, Global.player.moves[2])
+		turn[1] += 1
+		turn[0] = "opponent"
+
+
+func _on_Move4_pressed():
+	if turn[0] == "player" and turn[1] != 2:
+		BattleEffects.baseDamage(Global.player, Global.opponent, Global.player.moves[3])
+		turn[1] += 1
+		turn[0] = "opponent"
