b2922a30f2a140482b7261cb20c9bc0099a53f05
Xivius <nsong4@hawk.iit.edu>
2020-06-14 19:12:45 -0500

    Add play again option

--- End.gd
+++ End.gd
@@ -15,3 +15,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Button_pressed():
+	Global.id = 1
+	get_tree().change_scene("res://PokemonSelect/PokemonSelect.tscn")
